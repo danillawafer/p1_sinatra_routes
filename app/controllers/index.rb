0a2abf9f8ba0dfa150a242bc54345d7d306c0bc7
Apprentice <apprentice@dbc15.local>
2013-08-06 16:32:42 -0700

    route for all

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -2,3 +2,9 @@ get '/' do
   # Look in app/views/index.erb
   erb :index
 end
+
+get '/notes' do
+  # show all notes
+  @all_notes = Note.all
+  erb :notes
+end
---
+++ db/seeds.rb
@@ -0,0 +1,3 @@
+Note.create!(title: 'Note 1', content: 'Do your laundry')
+Note.create!(title: 'Note 2', content: 'Do your dishes')
+Note.create!(title: 'Note 3', content: 'Do your duty')
