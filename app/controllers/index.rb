efeaa8f12b551ebd4161e2c9bbe97765d96b30fc
DBC-Apprentice <dbc@dbc.com>
2013-08-06 19:16:19 -0700

    add update

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -10,18 +10,33 @@ get '/notes' do
 end
 
 get '/notes/new' do
-  
   # send user to form for creating note`
-
   erb :create_note
 end
 
 post '/notes' do
   Note.create!(params)
   # create a new note from form
-
   redirect '/notes'
 end
 
+get '/notes/:id' do 
+  puts params
+  @note = Note.find(params[:id]) 
+  erb :show_note
+end
+
+get '/notes/:id/edit' do
+  puts params
+  @note = Note.find(params[:id])
+  erb :update_note
+end
+
+put '/notes/:id' do
+  puts params
+  @note = Note.find(params[:id])
+  @note.update_attributes(params[:note])
+  redirect '/notes'
+end
 
 
